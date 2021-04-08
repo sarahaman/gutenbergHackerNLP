4f5c6af288c7817f82861c418cb768222880732d
Sara Haman <65554853+sarahaman@users.noreply.github.com>
2021-04-08 18:56:14 -0400

    Added to the documentation

--- Complexity Analysis/complexityAnalyses.py
+++ Complexity Analysis/complexityAnalyses.py
@@ -15,17 +15,22 @@
 from ImportTest import get_books, get_blogs, get_fics
 from TokenizeTest import TokenizeThis
 from tidyRoutine import TidyThis
-from pyspark.sql.functions import udf, col, count, countDistinct
+from pyspark.sql.functions import udf, col, count, countDistinct, sum
 
 tokenizedData = TokenizeThis(sc, "All")
 tidiedData = TidyThis(tokenizedData, "Yes", "No")
 
+# Note: We chose to lemmatize the data before calculating these statistics for two reasons: 
+#       1. Lemmatization (or stemming) was performed in the cleaning process for many of the resources on this topic that I read
+#       2. Lemmatization prevents artificial inflation of the complexity measures when variants of a root word (i.e., 
+#          walk, walks, walking) are used. 
+
 ##########  TYPE-TOKEN RATIO ########## 
 ''' The type-token ratio (TTR) represents the ratio of distinct words used in a document 
-to the total worrds used in the document. It serves as a measure of the complexity of
+to the total words used in the document. It serves as a measure of the complexity of
 the vocabulary in a document, in particular, the lexical diversity of the document.'''
 
-ttr = tidiedData.groupBy("Source").agg(countDistinct("Stem").alias("Unique"), count("Stem").alais("Total"))
+ttr = tidiedData.groupBy("Source").agg(countDistinct("Lemma").alias("Unique"), count("Lemma").alias("Total"))
 ttr = ttr.withColumn('Type-Token Ratio', (ttr['Unique']/ttr['Total']))
 ttr.show(3)
 
@@ -33,7 +38,7 @@ ttr.show(3)
 ''' The Hapax richness is a linguistic measure that calculates the ratio of words 
 used only once over the total number of words in the document.'''
  
-numWords = tidiedData.groupBy("Source", "Stem").agg(count("Stem").alias("Count"))
+numWords = tidiedData.groupBy("Source", "Lemma").agg(count("Lemma").alias("Count"))
 oneWord = numWords.filter(numWords["Count"] == 1)
 oneWord.groupBy("Source").agg(count("Count").alias("Hapax")).show()
 
@@ -41,7 +46,7 @@ oneWord.groupBy("Source").agg(count("Count").alias("Hapax")).show()
 '''Average word length represents the average length of words in the document.'''
 
 from fleschKincaid import item_length
-avgWL = tidiedData.select("Source", "Stem", item_length("Stem").alias("Len"))
+avgWL = tidiedData.select("Source", "Lemma", item_length("Stem").alias("Len"))
 avgWL = avgWL.withColumn("Len", avgWL.Len.cast('int'))
-avgWL2 = avgWL.groupBy("Source").agg(pyspark.sql.functions.sum("Len").alias("GroupLen"), count("Stem").alias("Tot$
+avgWL2 = avgWL.groupBy("Source").agg(sum("Len").alias("GroupLen"), count("Lemma").alias("Total"))
 avgWL2.withColumn("Average Word Length", (avgWL2["GroupLen"]/avgWL2["Total"])).show(3)
