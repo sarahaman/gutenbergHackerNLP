72213aac1ca340eb5734ba132dee5af257f88e97
Sara Haman <65554853+sarahaman@users.noreply.github.com>
2021-04-08 21:52:55 -0400

    Weird Error ??? Big Deal
    For some reason the server pushed a really, really old version of ImportTest.py to GitHub? Fixed it with the most recent version.

--- Data Cleaning/ImportTest.py
+++ Data Cleaning/ImportTest.py
@@ -2,6 +2,7 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
 import pandas as pd
+import pyspark.sql.functions as f
 import os
 import re
 
@@ -32,21 +33,43 @@ def get_books(sc):
   # return booksDF
 
 def get_blogs(sc):
-    '''
-    Arguments: sc
+   '''
+   Arguments: sc
         pyspark spark context obeject
+   Returns: rdd
+        cleaned spark rdd of the hacker_news_sample.csv and the blogtext.csv
+   '''
 
-    Returns: rdd
-        cleaned spark rdd of the hacker_news_sample.csv
+   spark = SparkSession.builder.getOrCreate()
+   newstextDF = spark.read.csv("/spring2021/project1/hacker_news_sample.csv", inferSchema = True, header = True).dropna(subset=['text'])
+   blogtextDF = spark.read.csv("/spring2021/project1/blogtext.csv", inferSchema = True, header = True).dropna(subset=['text'])
+
+   blogtextDF = blogtextDF.select('text')
+   newstextDF = newstextDF.select('text')
+   webtext = newstextDF.union(blogtextDF)
+
+   def cleanThis(x):
+      import re
+      x = re.sub('[<@*&?].*[>@*&?]','',x)
+      x = re.sub('[&@*&?].*[;@*&?]','',x)
+      x = re.sub('[#@*&?].*[;@*&?]','',x)
+      output = re.sub(r'[^0-9a-zA-z,.!?]+', ' ', x)
+      return output
+
+
+   cleanThis = f.udf(cleanThis)
+   cleanText = webtext.select(cleanThis('text').alias('text'))
+   web_rdd = cleanText.rdd.flatMap(list)
+   return web_rdd
+
+def get_fics(sc):
     '''
-    tempDataFrame = pd.read_csv("/spring2021/project1/hacker_news_sample.csv",
-                                error_bad_lines=False,
-                                engine='python').dropna(subset=['text'])
+    '''
+    fanficRDDs = []
+    fanfics =  os.listdir('/spring2021/project1/gemini/fanficComparison/')
 
-    tempDataFrame['text'] = [re.sub('[<@*&?].*[>@*&?]','',str(x)) for x in tempDataFrame["text"]]
-    tempDataFrame['text'] = [re.sub('[&@*&?].*[;@*&?]','',str(x)) for x in tempDataFrame["text"]]
-    tempDataFrame['text'] = [re.sub('[#@*&?].*[;@*&?]','',str(x)) for x in tempDataFrame["text"]]
+    for fic in fanfics:
+        fanficRDDs.append(sc.textFile("".join(['/spring2021/project1/gemini/fanficComparison/', fic])))
 
-    rdd = sc.parallelize(tempDataFrame['text'])
+    return fanficRDDs
 
-    return rdd
